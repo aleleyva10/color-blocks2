060e6b2d795736b565e815ba061e5fc29ddc4c5e
Alessandro Leyva <josealeyva87@gmail.com>
2017-07-19 15:25:15 -0500

    new changes

--- scripts.js
+++ scripts.js
@@ -1,36 +1,40 @@
+console.log('jQuery sourced');
+
 $(onReady);
 
-  function randomNumber(min, max) {
-    return Math.floor(Math.random() * (1 + max - min) + min);
-  }
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
 
-  function rgbValue() {
-    var r = randomNumber(0, 255);
-    var g = randomNumber(0, 255);
-    var b = randomNumber(0, 255);
-    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-  }
+function rgbValue() {
+  var r = randomNumber(0, 255);
+  var g = randomNumber(0, 255);
+  var b = randomNumber(0, 255);
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
 
-
-  var deleteBoxCircle = function() {
-    $(this).parent().remove();
-    console.log('deleted box or circle');
-  };
-
-
-  var addBlock = function() {
-    var $block = ('<div style="background-color: ' + rgbValue() + '"></div>');
-    $('.blocks').append($block);
-  };
-
-  var addCircle = function() {
-    var $circle = ('<div style="background-color: ' + rgbValue() + '"></div>');
-    $('.circles').append($circle);
-  };
+var addCircle = function() {
+  var $block = ('<div style="background-color:' + rgbValue() + '"></div>');
+  $('main').append($block);
+  updateCounters();
+};
 
 
-  function onReady() {
-    $('#blockButton').on("click", addBlock);
-    $('#circleButton').on("click", addCircle);
-    $('main').on('click', deleteBoxCircle);
-  }
+var deleteCircle = function() {
+  console.log('deleted circle');
+  $(this).remove();
+  updateCounters();
+};
+
+
+var updateCounters = function() {
+  console.log('in updateCounters');
+
+  $('#circles').text($('div').length);
+
+};
+
+function onReady() {
+  $('button').on('click', addCircle);
+  $('main').on('click', 'div', deleteCircle);
+}
